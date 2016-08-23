0c45917683f62eb650561b1e5aa0a64b76bd22d5
Rob Muhlestein <whitman.colm@gmail.com>
2016-08-23 09:10:39 -0400

    Now with editor I guess

--- Flashy/EditorViewController.swift
+++ Flashy/EditorViewController.swift
@@ -9,8 +9,61 @@
 import UIKit
 
 class EditorViewController: UIViewController{
+    // The view controller for the flashy editor and customizer.
+    
+    ////////////////////////////////////////
+    // And now, some wonderful IBOutlets: //
+    ////////////////////////////////////////
+    @IBOutlet weak var cardsetNameOutlet: UITextField!
+    @IBOutlet weak var cardsetShortOutlet: UITextField!
+
+    @IBOutlet weak var sideOneOutlet: UITextField!
+    @IBOutlet weak var sideTwoOutlet: UITextField!
+    
+    @IBOutlet weak var cardNumberOutlet: UILabel!
+    @IBOutlet weak var saveQuitOutlet: UIButton!
+    @IBOutlet weak var quitOnlyOutlet: UIButton!
+    @IBOutlet weak var previousOutlet: UIButton!
+    @IBOutlet weak var nextOutlet: UIButton!
+    ///////////////////////////////////////////////////////
+    // Fresh home-made functions and variables! By hand! //
+    ///////////////////////////////////////////////////////
+    func updateValuesInView() {
+        // Update text according to variables
+        
+        // Updating color scheme
+        view.backgroundColor = currentlySelectedColorScheme.backgroundColor
+    }
+    
+    //////////////////////////
+    // The viewDidLoad func //
+    //////////////////////////
+    
     override func viewDidLoad() {
          // Do any additional setup after loading the view, typically from a nib.
+        
+        updateValuesInView()
     }
+    
+    /////////////////////////////////////////////////
+    // Get your brand new IBAction functions here! //
+    /////////////////////////////////////////////////
+    @IBAction func saveQuitAction() {
+    }
+    
+    @IBAction func quitOnlyAction() {
+    }
+    
+    @IBAction func previousAction() {
+    }
+    
+    @IBAction func nextAction() {
+    }
+    
+    @IBAction func cardsetNameAction() {
+        print("Editing did end")
+    }
+    
+    
 }
 
