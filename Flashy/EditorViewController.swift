f2feedce32f38ccf157981ba8d46b01083f05e09
Rob Muhlestein <whitman.colm@gmail.com>
2016-08-23 07:30:12 -0400

    Oh boy this is going to be a long day

---
+++ Flashy/EditorViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditorViewController.swift
+//  Flashy
+//
+//  Created by Whitman Huntley on 8/23/16.
+//  Copyright © 2016 HyperChicken Interactive. All rights reserved.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController{
+    override func viewDidLoad() {
+         // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
