3fc4aa2cfb5c15261a4eaea274ffa0c0efd6a67f
Donovan Keohane <pinoycoding@gmail.com>
2016-10-26 19:38:08 -0400

    save

--- Flashy/SaveModel.swift
+++ Flashy/SaveModel.swift
@@ -8,3 +8,24 @@
 
 import Foundation
 
+
+let file = "flashyData.json"
+let flashySets = [set01, set02, set03, set04, set05] // Yes Whitman, I understand it needs to be dynamic. I'll add that later.
+var flashySetNames = [String]()
+
+func populateSetNames(_ names: Array<String>, _ sets: Array<AnyObject>) {
+    for set in sets {
+        var shortname: String = sets.shortName
+    }
+}
+
+func parse(_ sets: Array<String>) -> Array<String> {
+    let data = [String]()
+    
+    
+    
+    
+    
+    print(data)
+    return data
+}
