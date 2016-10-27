525d694c24e9a451bad5348ecc08cefd98899c82
Donovan Keohane <pinoycoding@gmail.com>
2016-10-27 18:32:14 -0400

    Started SaveModel.swift stuff

--- Flashy/SaveModel.swift
+++ Flashy/SaveModel.swift
@@ -6,26 +6,35 @@
 //  Copyright © 2016 HyperChicken Interactive. All rights reserved.
 //
 
+// WIP by @donovank
+// *Now with documentation!!!* //
+
 import Foundation
 
 
 let file = "flashyData.json"
 let flashySets = [set01, set02, set03, set04, set05] // Yes Whitman, I understand it needs to be dynamic. I'll add that later.
-var flashySetNames = [String]()
 
-func populateSetNames(_ names: Array<String>, _ sets: Array<AnyObject>) {
-    for set in sets {
-        var shortname: String = sets.shortName
-    }
+/**
+ Grabs *name* and *shortName* of a FlashySet, returns two variables.
+
+ _let name, shortname = grabName(set01)_
+*/
+
+func grabName(cset: inout FlashySet) -> (String, String) {
+    return (cset.name!, cset.shortName)
 }
 
-func parse(_ sets: Array<String>) -> Array<String> {
-    let data = [String]()
+/**
+ WIP, currently saves names to a json file.
+*/
+
+func saveToJson(_ names: Array<String>, count index: Int) {
+    //let name = names[index]
     
+    let flashCardData: [String: Int] = [
+        "id": 1
+        //"name": name
+    ]
     
-    
-    
-    
-    print(data)
-    return data
 }
